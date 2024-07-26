680f4a33282847a6d39b4dc5e6753afbbf82fa74
LYoungSoo <leeyeong04@gmail.com>
2024-07-26 17:45:21 +0900

    주말동안 List Set Map 검녹색 자바책에서 읽어볼것 분홍책은 설명이 구린건지 이해가 안되더라

---
+++ 04_Java/09_collection/src/list/run/ListRun.java
@@ -0,0 +1,11 @@
+package list.run;
+
+import list.service.ListService;
+
+public class ListRun {
+    public static void main(String[] args) {
+        ListService listService = new ListService();
+//        listService.test1();
+        listService.test2();
+    }
+}
